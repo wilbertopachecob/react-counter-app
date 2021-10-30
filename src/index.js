b1ca1e5486102aa9cf239664f1529f127eff4cf8
Wilberto Pacheco Batista <wilbertopachecob@gmail.com>
2021-10-30 18:42:09 -0500

    added font-awesome css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 import App from "./App.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
